0750c70a39c1b9bb8f6776d0fc0e7e89071c413b
Paulina Corcoran <paulinacorcoran@Paulinas-MacBook-Pro.local>
2020-03-30 14:28:11 -0400

    debug #4 toggle p1 element jQuery and change css for p2 and p3

---
+++ class/Class9-2/js/jQuery2.js
@@ -0,0 +1,44 @@
+$(document).ready( function(){
+
+
+$("#_hide").click(function(){
+  $(".gridContent div:first-child p").hide();
+});
+
+$("#_show").click(function(){
+  $(".gridContent div:first-child p").show();
+});
+
+$("#_toggle").click( function(){
+  $(".gridContent div:nth-child(2) p").toggle();
+});
+
+$("#_html").click(function() {
+  $(".gridContent div:nth-child(3) p").html("Hello <b>World</b>!");
+});
+
+$("#_css").click(function(){
+  $(".box").css("background-color", "lightblue");
+});
+
+
+$("footer").mouseover(function(){
+    $("footer p").html("Yeah! You're over me");
+});
+
+$("footer").mouseleave(function() {
+  $("footer p").html("Comeback...=(");
+});
+
+
+
+// --------------------- Debug #4 ----------------------------
+
+// Here goes your jQuery :
+// ---------------- toggle first p element --------------
+$("#toggleP").click( function(){
+  $(".smallGridBug div:first-child p").toggle();
+
+});
+
+});
