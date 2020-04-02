465a0a61b9de3abb3272ae974831f4ce3c04d9bc
Paulina Corcoran <paulinacorcoran@Paulinas-MacBook-Pro.local>
2020-04-02 07:12:26 -0400

    jQuery Assignment A

--- jQueryA/jQuery/jQA.js
+++ jQueryA/jQuery/jQA.js
@@ -1,33 +1,41 @@
 $(document).ready( function(){
 
 
-$("#Intro").click(function(){
-  $(".gridContent div:first-child p").hide();
-});
-
-$("#Travel").click(function(){
-  $(".gridContent div:first-child p").show();
-});
-
-$("#Weather").click( function(){
-  $(".gridContent div:nth-child(2) p").toggle();
-});
-
-$("#Hotels").click(function() {
-  $(".gridContent div:nth-child(3) p").html("Hello <b>World</b>!");
-});
-
-$("#Activities").click(function(){
-  $(".box").css("background-color", "lightblue");
-});
+  $("#WhyDR").click( function(){
+    $(".intro h1").html("¡Bienvenidos a la República Dominicana!");
+    $(".intro h1").toggle();
+    $(".intro h1").css("color", "red");
+  });
 
 
-$("footer").mouseover(function(){
-    $("footer p").html("Yeah! You're over me");
-});
+  $("#Travel").click( function(){
+    $(".gridContent div:first-child p").show();
+    $(".gridContent div:nth-child(2) p").hide();
+    $(".gridContent div:nth-child(3) p").hide();
+    $(".gridContent div:nth-child(4) p").hide();
+  });
 
-$("footer").mouseleave(function() {
-  $("footer p").html("Comeback...=(");
-});
+  $("#Weather").click( function(){
+    $(".gridContent div:first-child p").hide();
+    $(".gridContent div:nth-child(2) p").show();
+    $(".gridContent div:nth-child(3) p").hide();
+    $(".gridContent div:nth-child(4) p").hide();
+  });
+
+  $("#Hotels").click( function() {
+    $(".gridContent div:first-child p").hide();
+    $(".gridContent div:nth-child(2) p").hide();
+    $(".gridContent div:nth-child(3) p").show();
+    $(".gridContent div:nth-child(4) p").hide();
+  });
+
+  $("#Food").click( function(){
+    $(".gridContent div:first-child p").hide();
+    $(".gridContent div:nth-child(2) p").hide();
+    $(".gridContent div:nth-child(3) p").hide();
+    $(".gridContent div:nth-child(4) p").show();
+  });
+
+
 
 });
