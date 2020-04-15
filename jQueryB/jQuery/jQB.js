6b54d2ebc7e4aaa02cf452c175f5839a4ce99cc4
Paulina Corcoran <paulinacorcoran@Paulinas-MacBook-Pro.local>
2020-04-15 16:25:00 -0400

    jQuery part B

---
+++ jQueryB/jQuery/jQB.js
@@ -0,0 +1,45 @@
+$(document).ready( function(){
+
+  $('#pagepiling').pagepiling();
+
+console.log("hi");
+
+
+  $("#WhyDR").click( function(){
+    $(".intro h1").html("¡Bienvenidos a la República Dominicana!");
+    $(".intro h1").toggle();
+    $(".intro h1").css("color", "red");
+  });
+
+
+  $("#Travel").click( function(){
+    $(".gridContent div:first-child p").show();
+    $(".gridContent div:nth-child(2) p").hide();
+    $(".gridContent div:nth-child(3) p").hide();
+    $(".gridContent div:nth-child(4) p").hide();
+  });
+
+  $("#Weather").click( function(){
+    $(".gridContent div:first-child p").hide();
+    $(".gridContent div:nth-child(2) p").show();
+    $(".gridContent div:nth-child(3) p").hide();
+    $(".gridContent div:nth-child(4) p").hide();
+  });
+
+  $("#Hotels").click( function() {
+    $(".gridContent div:first-child p").hide();
+    $(".gridContent div:nth-child(2) p").hide();
+    $(".gridContent div:nth-child(3) p").show();
+    $(".gridContent div:nth-child(4) p").hide();
+  });
+
+  $("#Food").click( function(){
+    $(".gridContent div:first-child p").hide();
+    $(".gridContent div:nth-child(2) p").hide();
+    $(".gridContent div:nth-child(3) p").hide();
+    $(".gridContent div:nth-child(4) p").show();
+  });
+
+
+
+});
